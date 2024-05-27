c61490847a4ab1aebe1bbf5942281ef6e73f32f8
pythoninmakesinfo <159228628+pythoninmakesinfo@users.noreply.github.com>
2024-05-27 15:30:07 +0530

    Update urls.py

--- credentials/urls.py
+++ credentials/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile')
 ]
 
 
