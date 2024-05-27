ecce47770f4cdc8acb30988e4fc61cb0ee6649ab
pythoninmakesinfo <159228628+pythoninmakesinfo@users.noreply.github.com>
2024-05-27 15:31:38 +0530

    Update context_processors.py

--- moviewebsiteapp/context_processors.py
+++ moviewebsiteapp/context_processors.py
@@ -4,3 +4,7 @@ from .models import Genres
 def menu_links(request):
     links=Genres.objects.all()
     return dict(links=links)
+def genre_list(request):
+    return {
+        'genre_list': Genres.objects.all()
+    }
