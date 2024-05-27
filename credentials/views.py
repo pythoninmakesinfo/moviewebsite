7c19e97fb1332544fbe8703549093b6fcff414fc
pythoninmakesinfo <159228628+pythoninmakesinfo@users.noreply.github.com>
2024-05-27 15:28:58 +0530

    Update views.py

--- credentials/views.py
+++ credentials/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm, LoginForm
+from .forms import UserRegistrationForm, UserProfileForm
 from django.contrib import messages
 
 
@@ -9,12 +10,11 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration successful.')
-            return redirect('/')
+            form.save()
+            messages.success(request, 'Registration successful. Please log in.')
+            return redirect('credentials:login')  # Redirect to login page
         else:
-            messages.error(request, 'Registration failed. Please correct the error below.')
+            messages.error(request, 'Registration failed. Please correct the errors below.')
     else:
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
@@ -27,7 +27,7 @@ def login_view(request):
             user = form.get_user()
             login(request, user)
             messages.success(request, 'Login successful.')
-            return redirect('/')
+            return redirect('/')  # Redirect to the homepage
         else:
             messages.error(request, 'Invalid username or password.')
     else:
@@ -39,3 +39,15 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'Logout successful.')
     return redirect('/')
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        user_form = UserProfileForm(request.POST, instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('credentials:profile')
+    else:
+        user_form = UserProfileForm(instance=request.user)
+    return render(request, 'profile.html', {'user_form': user_form})
+
