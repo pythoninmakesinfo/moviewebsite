e4ad65dada275f72812533bed89fe40616e117a7
pythoninmakesinfo <159228628+pythoninmakesinfo@users.noreply.github.com>
2024-05-27 15:27:20 +0530

    Update forms.py

--- credentials/forms.py
+++ credentials/forms.py
@@ -17,3 +17,11 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(required=True)
     password = forms.CharField(widget=forms.PasswordInput, required=True)
 
+class UserProfileForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(max_length=254, required=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
