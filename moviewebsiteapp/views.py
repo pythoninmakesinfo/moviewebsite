f9827528fc7fe803347ca20f0ccc640463d507fd
pythoninmakesinfo <159228628+pythoninmakesinfo@users.noreply.github.com>
2024-05-28 11:00:50 +0530

    Update views.py

--- moviewebsiteapp/views.py
+++ moviewebsiteapp/views.py
@@ -1,30 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import MovieForm
-from .models import Movie
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import MovieForm, ReviewForm
+from .models import Movie, Review, Genres
 from django.contrib import messages
-from .forms import  ReviewForm
 from django.contrib.auth.decorators import login_required
-from .models import  Review, Genres
-
 
 def index(request):
-    moviess=Movie.objects.all()
-    context={'Movie_list':moviess}
+    moviess = Movie.objects.all()
+    context = {'Movie_list': moviess}
     return render(request, 'index.html', context)
 
-
-def detail(request,movie_id):
-    moviess =get_object_or_404(Movie, id=movie_id)
+def detail(request, movie_id):
+    moviess = get_object_or_404(Movie, id=movie_id)
     reviews = Review.objects.filter(movie=moviess)
-    genres=Genres.objects.filter(movie=moviess)
-    return render(request,"detail.html", {'movies': moviess,'reviews':reviews,'genres':genres})
-    return HttpResponse("this is movies no %s"% movie_id)
+    genres = Genres.objects.filter(movie=moviess)
+    return render(request, "detail.html", {'movies': moviess, 'reviews': reviews, 'genres': genres})
+
 @login_required
 def youtube_trailer(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
     return render(request, "youtube_trailer.html", {'movies': movie})
-
 
 @login_required
 def add_movie(request):
@@ -34,14 +28,14 @@ def add_movie(request):
             movie = form.save(commit=False)
             movie.added_by = request.user
             movie.save()
+            messages.success(request, 'Movie added successfully!')
             return redirect('/')
     else:
         form = MovieForm()
     return render(request, 'add.html', {'form': form})
 
-
 @login_required
-def update(request, id):  # Updated here
+def update(request, id):
     movie = get_object_or_404(Movie, id=id)
     if movie.added_by != request.user:
         messages.error(request, 'You do not have permission to update this movie.')
@@ -58,7 +52,7 @@ def update(request, id):  # Updated here
     return render(request, 'edit.html', {'form': form, 'movie': movie})
 
 @login_required
-def delete(request, id):  # Updated here
+def delete(request, id):
     movie = get_object_or_404(Movie, id=id)
     if movie.added_by != request.user:
         messages.error(request, 'You do not have permission to delete this movie.')
@@ -69,6 +63,7 @@ def delete(request, id):  # Updated here
         messages.success(request, 'Movie deleted successfully!')
         return redirect('/')
     return render(request, 'delete.html', {'movie': movie})
+
 @login_required
 def add_review(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
@@ -79,18 +74,18 @@ def add_review(request, movie_id):
             review.movie = movie
             review.user = request.user
             review.save()
+            messages.success(request, 'Review added successfully!')
             return redirect('moviewebsiteapp:detail', movie_id=movie_id)
     else:
         form = ReviewForm()
     return render(request, 'add_review.html', {'form': form, 'movie': movie})
+
 @login_required
 def movies_by_genre(request, genre_id):
     genre = get_object_or_404(Genres, id=genre_id)
     movies = Movie.objects.filter(genres=genre)
-    return render(request, 'genres.html', {'genre': genre, 'movies': movies})
-
-
-
+    return render(request, 'movies_by_genre.html', {'genre': genre, 'movies': movies})
+    
 
 
 
